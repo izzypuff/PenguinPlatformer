1a53e7d999332c8b1cae398c86c293aa621fdc87
Isabel Morimoto <isabelmorimoto@gmail.com>
2023-10-22 17:14:45 -0400

    added comments and changed name in project settings

--- Assets/Scripts/LevelStart.cs
+++ Assets/Scripts/LevelStart.cs
@@ -5,21 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class LevelStart : MonoBehaviour
 {
-    //loads level 1 scene
     public void LoadNewScene()
     {
+        //gets active scene
         Scene scene = SceneManager.GetActiveScene();
 
+        //if active scene is level 1 start
         if (scene.name == "Level1Start")
         {
+            //load level 1
             SceneManager.LoadScene("Level1");
         }
+        //if active scene is load level 2 start
         if (scene.name == "Level2Start")
         {
+            //load level 2
             SceneManager.LoadScene("Level2");
         }
+        //if active scene is end start
         if (scene.name == "EndStart")
         {
+            //load end
             SceneManager.LoadScene("End");
         }
     }
